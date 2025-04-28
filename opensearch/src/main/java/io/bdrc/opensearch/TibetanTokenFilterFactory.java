4c4d27d9c1b4e24c856653b9ac3fa63a2b7f89d0
Elie Roux <roux.elie@gmail.com>
2025-04-28 15:01:11 +0200

    new version with lucene-bo update

--- opensearch/src/main/java/io/bdrc/opensearch/TibetanTokenFilterFactory.java
+++ opensearch/src/main/java/io/bdrc/opensearch/TibetanTokenFilterFactory.java
@@ -9,28 +9,33 @@ import org.opensearch.index.analysis.AbstractTokenFilterFactory;
 import io.bdrc.lucene.bo.PaBaFilter;
 import io.bdrc.lucene.bo.TibAffixedFilter;
 import io.bdrc.lucene.bo.TibSyllableLemmatizer;
+import io.bdrc.lucene.bo.YiTokenFilter;
 
 public class TibetanTokenFilterFactory extends AbstractTokenFilterFactory {
     
     private boolean remove_affixes = false;
     private boolean normalize_paba = false;
     private boolean normalize_verbs = false;
+    private boolean remove_yi = false;
     
     public TibetanTokenFilterFactory(final IndexSettings indexSettings, final Environment env, final String name, final Settings settings) {
         super(indexSettings, name, settings);
         this.remove_affixes = settings.getAsBoolean("remove_affixes", false);
         this.normalize_paba = settings.getAsBoolean("normalize_paba", false);
         this.normalize_verbs = settings.getAsBoolean("normalize_verbs", false);
+        this.remove_yi = settings.getAsBoolean("remove_yi", this.remove_affixes); // by default, same as remove_affixes
     }
 
     @Override
     public TokenStream create(TokenStream tokenStream) {
         if (this.remove_affixes)
             tokenStream = new TibAffixedFilter(tokenStream, true);
-        if (this.normalize_verbs)
-            tokenStream = new TibSyllableLemmatizer(tokenStream);
+        if (this.remove_yi)
+        	tokenStream = new YiTokenFilter(tokenStream);
         if (this.normalize_paba)
             tokenStream = new PaBaFilter(tokenStream);
+        if (this.normalize_verbs)
+            tokenStream = new TibSyllableLemmatizer(tokenStream);
         return tokenStream;
     }
 
